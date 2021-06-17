b5f079c6ce8ecfe92c4e076abb7fc459a4690795
lcd1232 <8745863+lcd1232@users.noreply.github.com>
2021-06-17 16:14:29 +0300

    More tests

--- gtranslate_test.go
+++ gtranslate_test.go
@@ -33,8 +33,8 @@ func TestTranslateWithFromTo(t *testing.T) {
 	}
 }
 
-func TestTranslateAdvanced(t *testing.T) {
-	responseBody := `[[["Привет","hi",null,null,1]
+var responses = []string{
+	`[[["Привет","hi",null,null,1]
 ,[null,null,"Privet","hī"]
 ]
 ,[["сокращение",["Гавайи"]
@@ -66,26 +66,119 @@ func TestTranslateAdvanced(t *testing.T) {
 ]
 ,"hi"]
 ]
-]`
+]`,
+	`[[["Привет","hello",null,null,10]
+,[null,null,"Privet","həˈlō"]
+]
+,[["глагол",["здороваться","звать","окликать"]
+,[["здороваться",["greet","hello","salute","hallo","hullo","bow"]
+,null,0.0028530264]
+,["звать",["call","invite","shout","hail","hallo","hello"]
+,null,2.753645E-5]
+,["окликать",["hail","holler","call","challenge","speak","hello"]
+,null,2.753645E-5]
+]
+,"hello",2]
+,["имя существительное",["приветствие","приветственный возглас","возглас удивления"]
+,[["приветствие",["greeting","welcome","salute","salutation","hello","hail"]
+,null,0.0014801305,null,3]
+,["приветственный возглас",["hallo","halloa","hello","viva"]
+,null,2.753645E-5,null,1]
+,["возглас удивления",["hallo","halloa","hello"]
+,null,2.753645E-5]
+]
+,"hello",1]
+]
+,"en",null,null,[["hello",null,[["Привет",1000,true,false,[10,3,0]
+]
+,["приветствовать",1000,true,false,[10]
+]
+,["Здравствуйте",1000,true,false,[10]
+]
+]
+,[[0,5]
+]
+,"hello",0,0]
+]
+,1,[]
+,[["en"]
+,null,[1]
+,["en"]
+]
+,null,null,null,[["восклицание",[["used as a greeting or to begin a phone conversation.","m_en_gbus0460730.012","hello there, Katie!"]
+]
+,"hello"]
+,["имя существительное",[["an utterance of “hello”; a greeting.","m_en_gbus0460730.025","she was getting polite nods and hellos from people"]
+]
+,"hello"]
+,["глагол",[["say or shout “hello”; greet someone.","m_en_gbus0460730.034","I pressed the phone button and helloed"]
+]
+,"hello"]
+]
+,[[["\u003cb\u003ehello\u003c/b\u003e there, Katie!",null,null,null,3,"m_en_gbus0460730.012"]
+]
+]
+]
+`,
+}
 
-	WithTestServer(t, 200, []byte(responseBody), func(url string, requestBodyCh <-chan []byte) {
-		translated, err := TranslateAdvanced("hi", language.English, language.Russian, url)
-		require.NoError(t, err)
-		assert.Equal(t, Translated{
-			Original:              "hi",
-			OriginalLanguage:      "en",
-			OriginalPronunciation: "hī",
-			Text:                  "Привет",
-			Pronunciation:         "Privet",
-			Definitions: []string{
-				"used as a friendly greeting or to attract attention.",
-				"Hawaii (in official postal use).",
+func TestTranslateAdvanced(t *testing.T) {
+	for _, tc := range []struct {
+		name string
+		text string
+		body string
+		want Translated
+	}{
+		{
+			name: "hi",
+			text: "hi",
+			body: responses[0],
+			want: Translated{
+				Original:              "hi",
+				OriginalLanguage:      "en",
+				OriginalPronunciation: "hī",
+				Text:                  "Привет",
+				Pronunciation:         "Privet",
+				Definitions: []string{
+					"used as a friendly greeting or to attract attention.",
+					"Hawaii (in official postal use).",
+				},
+				Examples: []string{
+					"“Hi there. How was the flight?”",
+				},
 			},
-			Examples: []string{
-				"“Hi there. How was the flight?”",
+		},
+		{
+			name: "hello",
+			text: "hello",
+			body: responses[1],
+			want: Translated{
+				Original:              "hello",
+				OriginalLanguage:      "en",
+				OriginalPronunciation: "həˈlō",
+				Text:                  "Привет",
+				Pronunciation:         "Privet",
+				Definitions: []string{
+					"used as a greeting or to begin a phone conversation.",
+					"an utterance of “hello”; a greeting.",
+					"say or shout “hello”; greet someone.",
+				},
+				Examples: []string{
+					"hello there, Katie!",
+					"she was getting polite nods and hellos from people",
+					"I pressed the phone button and helloed",
+				},
 			},
-		}, translated)
-	})
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			WithTestServer(t, 200, []byte(tc.body), func(url string, requestBodyCh <-chan []byte) {
+				translated, err := TranslateAdvanced(tc.text, language.English, language.Russian, url)
+				require.NoError(t, err)
+				assert.Equal(t, tc.want, translated)
+			})
+		})
+	}
 }
 
 func WithTestServer(t *testing.T, responseCode int, responseBody []byte, f func(url string, requestBodyCh <-chan []byte)) {
